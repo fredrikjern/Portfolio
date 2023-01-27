60f62dfc7b839a53c240fb7699b63aa39aa775ca
fredrikjern <fjernberg@gmail.com>
2023-01-27 08:04:23 +0100

    Header animation

--- main.js
+++ main.js
@@ -2,21 +2,37 @@ let moveButton = document.querySelector(".move-button");
 //let wave_0 = document.querySelector(".wave-0");
 let waves = document.querySelectorAll(".wave");
 let backgroundGradient = document.querySelector(".div-waves");
-moveButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  transitionGradient(170, 278, -15, 115, -20, -30, 2000, backgroundGradient,waves);
-  waves.forEach((element, index) => {
-    if (index % 2 === 0) {
-      console.log("hej");
-      element.classList.toggle("translateX-20");
-    }
+let navlinks = document.querySelectorAll(".nav-link");
+navlinks.forEach((navlink) => {
+  navlink.addEventListener("click", (event) => {
+    event.preventDefault();
+    transitionGradient(
+      170,
+      290,
+      -15,
+      115,
+      -20,
+      -30,
+      3000,
+      backgroundGradient,
+      waves
+    );
   });
-  //wave_0.classList.toggle("translateX-20")
 });
 
 // Funktions
-function transitionGradient(startAngle,endAngle,startX,endX,startY,highY,time,obj,waves
-) { return new Promise((resolve, reject) => {
+function transitionGradient(
+  startAngle,
+  endAngle,
+  startX,
+  endX,
+  startY,
+  highY,
+  time,
+  obj,
+  waves
+) {
+  return new Promise((resolve, reject) => {
     let frameRate = 1000 / 60; // 16.666666666666668, uppdateringsfrekens
     let frames = Math.round(time / frameRate); //ger totala antalet frames under animationen? (avrundat)
     let updatePerFrame = (endAngle - startAngle) / frames;
@@ -29,25 +45,26 @@ function transitionGradient(startAngle,endAngle,startX,endX,startY,highY,time,ob
     let frame = 0;
 
     let intervalId = setInterval(function () {
-        frame++;
-        angle += updatePerFrame;
-        x += deltaX;
-        if (frame > frames / 2) {
-            increase = false;
-        }
-        y = increase ? y - deltaY : y + deltaY;
+      frame++;
+      angle += updatePerFrame;
+      x += deltaX;
+      if (frame > frames / 2) {
+        increase = false;
+      }
+      y = increase ? y - deltaY : y + deltaY;
 
-        obj.style.backgroundImage = `conic-gradient(from ${angle}deg at ${x}% ${y}%, rgb(130, 160, 238) 76%, rgb(182, 202, 255) 87%, rgb(130, 160, 238) 100%)`;
+      obj.style.backgroundImage = `conic-gradient(from ${angle}deg at ${x}% ${y}%, rgb(130, 160, 238) 76%, rgb(182, 202, 255) 87%, rgb(130, 160, 238) 100%)`;
 
-        if (frame % 20 == 0) {
-            waves.forEach((element, index) => {
-                if (frame % 20 === 0) {
-                    console.log("hej");
-                    element.classList.toggle("translateX-20");
-                }
-            });
-        }
-        if (angle >= endAngle) {
+      if (frame % 20 == 0) {
+        waves.forEach((element, index) => {
+            if (index % 2 === 0) {
+              element.classList.toggle("translateX20");
+            } else {
+                element.classList.toggle("translateX-20");
+            }
+        });
+      }
+      if (angle >= endAngle) {
         clearInterval(intervalId);
         resolve();
       }
